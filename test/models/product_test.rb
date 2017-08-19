fec4f0861295bfeb50d0089eba094504dc12f615
cwhite204 <cwhite31@qub.ac.uk>
2017-08-19 21:36:01 +0100

    Added store contoller and view

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,7 @@
-fixtures :products
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
 
   test "product attributes must not be empty" do
     product =  Product.new
@@ -49,6 +49,7 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} should'nt be valid"
     end
+  end
 
     test "product is not valid without a unique title" do
       product = Product.new(title: products(:ruby).title,
